cfcd2910d90ea093b629ba5cece6c6bc6912582e
DESKTOP-867UHHI\alice <alice90270@gmail.com>
2017-06-10 23:21:53 +0800

    effect 4 done (without removing background function)

--- effect4_scenery.m
+++ effect4_scenery.m
@@ -1,18 +1,23 @@
 clear
 close all
 clc
+%% Remove background
 
 %%  inpurt photos
-img = imresize(imread('photo.png'), 1);
-[M, N, P] = size(img);
-backg = imresize(im2single(imread('bg_scene.png')), 1);
-backg = backg(1:M, 1:N, :);
+% initial size = 500*500
+[img, map, alpha]= imread('photo', 'png');
+img= imresize(img, [500 500]);
+alpha= imresize(alpha, [500 500]);
+x=imshow(img, map);
 
-%% cut the face
-
-%% reduce the transparency and combine
-figure;
-image(backg);
+%% input background & combine
+backg = imread('bg_scene.png');
+backg= imresize(backg, [500 500]);
 hold on;
-head=image(img);
-set(head,'AlphaData',0.5);
+y=imshow(backg);
+
+%% cut the outline & reduce the transparency
+set(y, 'AlphaData', alpha*0.5);
+set(x, 'AlphaData', alpha);
+
+%% example
