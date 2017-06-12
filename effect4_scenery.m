8dc234daaad09f439a212c158596409f47b6da16
DESKTOP-867UHHI\alice <alice90270@gmail.com>
2017-06-12 21:52:39 +0800

    effet with color background

--- effect4_scenery.m
+++ effect4_scenery.m
@@ -1,23 +1,39 @@
+
 clear
 close all
 clc
 %% Remove background
+bg_color = imread('effect4_bg_color.png');
+bg_color = imresize(bg_color, [500 500]);
+a= figure;
+
 
 %%  inpurt photos
 % initial size = 500*500
 [img, map, alpha]= imread('photo', 'png');
 img= imresize(img, [500 500]);
 alpha= imresize(alpha, [500 500]);
-x=imshow(img, map);
+x=imshow(img, 'border','tight','initialmagnification','fit');set(gcf,'Position',[0,0,500,500]);axis normal;
+
 
 %% input background & combine
 backg = imread('bg_scene.png');
 backg= imresize(backg, [500 500]);
 hold on;
-y=imshow(backg);
+z= imshow(bg_color,'border','tight','initialmagnification','fit');set(gcf,'Position',[0,0,500,500]);axis normal;
+x= imshow(img,'border','tight','initialmagnification','fit');set(gcf,'Position',[0,0,500,500]);axis normal;
+y= imshow(backg,'border','tight','initialmagnification','fit');set(gcf,'Position',[0,0,500,500]);axis normal;
+
 
 %% cut the outline & reduce the transparency
 set(y, 'AlphaData', alpha*0.5);
 set(x, 'AlphaData', alpha);
 
-%% example
+%% Save image
+saveas(a,'result4.png');
+
+%imwrite(a, 'effect4.png');
+output = imread('result4', 'png');
+figure(2);
+output=imresize(output,[500 500]);
+imshow(output);
