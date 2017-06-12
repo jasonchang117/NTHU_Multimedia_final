9c7a5c6c8299ee13696262dee5ba881c9ffe640c
JASON-LAPTOP\JasonChang <jasonchang117@gmail.com>
2017-06-12 23:14:36 +0800

    combine GUI with effect4

---
+++ eff_landscape.m
@@ -0,0 +1,38 @@
+function [ output ] = eff_landscape( filename, cycle )
+
+%% Remove background
+bg_color = imread('effect4_bg_color.png');
+bg_color = imresize(bg_color, [500 500]);
+a= figure;
+
+
+%%  inpurt photos
+% initial size = 500*500
+[img, map, alpha]= imread(filename);
+img= imresize(img, [500 500]);
+alpha= imresize(alpha, [500 500]);
+x=imshow(img, 'border','tight','initialmagnification','fit');set(gcf,'Position',[0,0,500,500]);axis normal;
+
+
+%% input background & combine
+backg = imread( strcat('bg_scene', num2str(cycle), '.png'));
+backg= imresize(backg, [500 500]);
+hold on;
+z= imshow(bg_color,'border','tight','initialmagnification','fit');set(gcf,'Position',[0,0,500,500]);axis normal;
+x= imshow(img,'border','tight','initialmagnification','fit');set(gcf,'Position',[0,0,500,500]);axis normal;
+y= imshow(backg,'border','tight','initialmagnification','fit');set(gcf,'Position',[0,0,500,500]);axis normal;
+
+
+%% cut the outline & reduce the transparency
+set(y, 'AlphaData', alpha*0.3);
+set(x, 'AlphaData', alpha);
+
+%% Save image
+saveas(a,'result4.png');
+
+%imwrite(a, 'effect4.png');
+output = imread('result4', 'png');
+% figure(2);
+output=imresize(output,[500 500]);
+% imshow(output);
+end
